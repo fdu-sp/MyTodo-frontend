f0a6eda778e735155d519eba8353dc1768062328
zmark <351479188@qq.com>
2024-04-16 15:41:53 +0800

    feat: TaskList.vue 和 TodoPage.vue对接

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,6 +1,6 @@
 <template>
   <q-list bordered class="rounded-borders">
-    <q-item v-for="task in tasks" :key="task.id" clickable v-ripple @click="$emit('select-task', task.id)">
+    <q-item v-for="task in tasks" :key="task.id" clickable v-ripple @click="selectTask(task.id)">
       <q-item-section>
         <q-item-label>
           {{ task.title }}
@@ -15,18 +15,16 @@
   </q-list>
 </template>
 
-
 <script setup>
-import { defineProps } from 'vue';
+import {defineProps} from 'vue';
 
-// const props = defineProps({
-//   tasks: Array
-// });
+const props = defineProps({
+  tasks: Array
+});
+// 定义可以发射的事件列表
+const emit = defineEmits(['task-selected']);
 
-const tasks = [
-  { id: 1, title: '准备会议材料', dueDate: '今天', isImportant: true },
-  { id: 2, title: '邮件回复客户', dueDate: '明天', isImportant: false },
-  { id: 3, title: '完成报告', dueDate: '下周一', isImportant: true },
-  // 更多任务...
-];
+function selectTask(taskId) {
+  emit('task-selected', taskId);
+}
 </script>
