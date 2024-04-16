eae77349f20b31167549bdc08ce61f0e27f3b442
zmark <351479188@qq.com>
2024-04-16 16:04:36 +0800

    feat: TaskDetails.vue 和 TodoPage.vue对接

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -1,11 +1,11 @@
 <template>
-  <div v-if="taskDetails" class="task-details">
+  <div v-if="task" class="task-details">
     <!-- 任务标题 -->
-    <h6 :class="{'important': taskDetails.isImportant}">{{ taskDetails.title }}</h6>
+    <h6 :class="{'important': task.taskPriorityInfo.isImportant}">{{ task.title }}</h6>
     <!-- 任务描述 -->
-    <p>{{ taskDetails.description }}</p>
+    <p>{{ task.taskContentInfo.description }}</p>
     <!-- 任务截止日期 -->
-    <p class="due-date">Due Date: <strong>{{ taskDetails.dueDate }}</strong></p>
+    <p class="due-date">Due Date: <strong>{{ task.taskTimeInfo.endDate }}</strong></p>
     <!-- 更多任务详情可以在这里添加 -->
   </div>
   <div v-else class="no-task">
@@ -16,29 +16,19 @@
 
 
 <script setup>
-import { defineProps } from 'vue';
-import TaskDetails from "components/TaskDetails.vue";
+import {defineProps} from 'vue';
 
-// const props = defineProps({
-//   task: Object
-// });
-
-const taskDetails = {
-  id: 1,
-  title: '准备会议材料',
-  description: '收集所有项目进度报告，制作PPT。',
-  isImportant: true,
-  dueDate: '今天',
-  // 其他信息...
-};
-
+const props = defineProps({
+  task: Object
+});
 </script>
+
 <style scoped>
 .task-details {
   background-color: #f4f4f4;
   padding: 20px;
   border-radius: 10px;
-  box-shadow: 0 4px 6px rgba(0,0,0,0.1);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 
 .task-details .important {
