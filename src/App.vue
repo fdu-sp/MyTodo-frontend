b6301157cc2dd820b39a9865ca17fa3eb0c57af0
zmark <351479188@qq.com>
2024-04-16 14:25:17 +0800

    feat(hello): 弹出提醒

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,22 @@
 
 <script setup>
 import {helloRequest} from 'src/api/hello';
+import {useQuasar} from "quasar";
 
 defineOptions({
   name: 'App'
 });
 
+const $q = useQuasar();
 helloRequest()
   .then(data => {
-    // todo 弹出提示消息
-    console.log(data);
-    console.log(data.object);
+    $q.notify({
+      message: data.object,
+      type: "positive",
+      color: "primary",
+      position: "bottom",
+      timeout: 2000,
+    });
   })
   .catch(error => {
     console.error(error);
