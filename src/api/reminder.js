364884cecd3badab228b56cb8e70195c94a03e9b
zmark <351479188@qq.com>
2024-05-08 18:55:41 +0800

    feat(api): reminder.js 新增 getRecentReminders

--- src/api/reminder.js
+++ src/api/reminder.js
@@ -2,6 +2,15 @@ import {api} from 'boot/axios';
 
 // /api/reminder/get-all
 // 获取所有提醒信息
-export function getAllReminders(){
-    return api.get('/reminder/get-all');
+export function getAllReminders() {
+	return api.get('/reminder/get-all');
+}
+
+// /api/reminder/get-recent/{hour}
+/**
+ * @description 获取最近一段时间的提醒信息
+ * @param {number} hour 小时数
+ * */
+export function getRecentReminders(hour) {
+	return api.get(`/reminder/get-recent/${hour}`);
 }
