8802f1783dd957e4ffc104fe14bea4c70252ea65
zmark <351479188@qq.com>
2024-05-09 11:06:49 +0800

    refactor: 拆分出TaskInList

---
+++ src/components/TaskInList.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-item-section>
+    <q-item-label>
+      {{ task.title }}
+      <!-- 显示星号图标标识重要任务 -->
+      <q-icon name="star" v-if="task.isImportant" class="text-warning q-ml-sm"/>
+    </q-item-label>
+    <q-item-label caption>{{ task.description }}</q-item-label>
+    <!-- 显示任务截止日期 -->
+    <q-item-label caption class="text-grey-8">{{ task.dueDate }}</q-item-label>
+  </q-item-section>
+</template>
+
+<script setup>
+const props = defineProps({
+  task: Object
+});
+</script>
+
+<style scoped>
+
+</style>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,21 +1,14 @@
 <template>
   <q-list bordered class="rounded-borders">
     <q-item v-for="task in tasks" :key="task.id" clickable v-ripple @click="selectTask(task.id)">
-      <q-item-section>
-        <q-item-label>
-          {{ task.title }}
-          <!-- 显示星号图标标识重要任务 -->
-          <q-icon name="star" v-if="task.isImportant" class="text-warning q-ml-sm"/>
-        </q-item-label>
-        <q-item-label caption>{{ task.description }}</q-item-label>
-        <!-- 显示任务截止日期 -->
-        <q-item-label caption class="text-grey-8">{{ task.dueDate }}</q-item-label>
-      </q-item-section>
+      <task-in-list :task="task"/>
     </q-item>
   </q-list>
 </template>
 
 <script setup>
+
+import TaskInList from "components/TaskInList.vue";
 
 const props = defineProps({
   tasks: Array
